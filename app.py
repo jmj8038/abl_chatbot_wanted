28c4e167807faed7087325bb806ea15a523ca251
minjae jin <jmj8038@gmail.com>
2023-09-25 10:21:20 +0900

    url host chage with ngrok 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import streamlit as st
 
 #API_BASE_URL = "http://localhost:8000/chat"
-API_BASE_URL = https://22e4-218-38-115-107.ngrok.io/chat 
+API_BASE_URL = "https://22e4-218-38-115-107.ngrok.io/chat"
 
 #st.title("ABL AI ChtBot")
 
