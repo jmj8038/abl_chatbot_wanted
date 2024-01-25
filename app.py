4dc8a0ca8c15a51be6009b3ae22f6214e183c0b7
minjae jin <jmj8038@gmail.com>
2024-01-25 13:45:13 +0900

    '20240125_4'

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import time
 import requests
 import streamlit as st
 
-API_BASE_URL = "http://localhost:8000/chat"
-#API_BASE_URL = 'https://ffb2-218-38-21-23.ngrok-free.app/chat'
+#API_BASE_URL = "http://localhost:8000/chat"
+API_BASE_URL = 'https://ffb2-218-38-21-23.ngrok-free.app/chat'
 #st.title("ABL AI ChtBot")
 
 contracts = ['주계약', '무배당 경도이상치매진단특약T(해약환급금 미지급형)',
